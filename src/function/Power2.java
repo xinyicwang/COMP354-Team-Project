f234930e625386abbb29f8280c5eef4040a54033
Briana Toia <55514975+bbtoia@users.noreply.github.com>
2020-06-02 23:12:27 -0400

    fixed code to have a public method to call for pi^x
    - removed main
    - fixed spacing (I think?)

--- src/function/Power2.java
+++ src/function/Power2.java
@@ -3,137 +3,138 @@ package function;
 public class Power2 {
 
 	 static final double EPSILON = 0.000001;
-	    
-	    
-		public static void main(String[] args) 
+	        
+	/**
+     	* Function that calculates pi^x
+     	* @param exp: exponent
+     	* @return pi raised to the power of exp
+     	*/
+	public static double powersOfPi(double exp)
+	{
+		double pi = 0;
+		double sign = 1;
+		int limit = 100000000;
+
+		for (int i = 1; i < limit; i+=2)
 		{
-			double pi = 0;
-			double sign = 1;
-			int limit = 100000000;
-
-			for (int i = 1; i < limit; i+=2)
-			{
-			   pi += sign/i;
-			   sign = -sign;
-			}
-			pi *=4;
-
-			System.out.println(pi);
-			System.out.println(power(pi,100));
-
+		   pi += sign/i;
+		   sign = -sign;
 		}
+		pi *=4;
+
+		return power(pi, exp);
+	}
 		
 		
-		/**
-		 * Function that calculates the power of a number raised to an exponent
-		 * @param base: base number
-		 * @param exp: exponent
-		 * @return the result of base multiplied by itself exp times
-		 */
-		public static double power(double base, double exp)
-		{
-			long nthRoot = 1;
-			boolean isNegative = exp < 0;
+	/**
+	* Function that calculates the power of a number raised to an exponent
+	* @param base: base number
+	* @param exp: exponent
+	* @return the result of base multiplied by itself exp times
+	*/
+	public static double power(double base, double exp)
+	{
+		long nthRoot = 1;
+		boolean isNegative = exp < 0;
 			
-			exp = isNegative ? exp*(-1) : exp;
+		exp = isNegative ? exp*(-1) : exp;
 
-			//checks if the exponent is not an integer
-			//if not, multiplies by 10 until it is, resulting in a fraction of 
-			//exp/nthRoot being the new exponent
-			while (exp%1 != 0)
-			{
-				nthRoot *= 10;
-				exp *= nthRoot;
-			}
-
-			base = root(base, nthRoot);
-
-			double powerResult = 1;
-
-			for (int i = 0; i < exp; i++)
-			{
-				powerResult *= base;
-			}
-					
-			return isNegative ? 1/powerResult : powerResult;
-		}
-		
-		
-		/**
-		 * Finds the nth root by approximation
-		 * @param base: number to approximate
-		 * @param nthRoot: number of times the root needs to be multiplied by itself 
-		 * to result the base 
-		 * @return the nth root of the base
-		 */
-		private static double root(double base, double nthRoot)
+		//checks if the exponent is not an integer
+		//if not, multiplies by 10 until it is, resulting in a fraction of 
+		//exp/nthRoot being the new exponent
+		while (exp%1 != 0)
 		{
-		    if (nthRoot == 1)
-		    {
-		        return base;
-		    }
+			nthRoot *= 10;
+			exp *= nthRoot;
+		}
+
+		base = root(base, nthRoot);
+		double powerResult = 1;
+
+		for (int i = 0; i < exp; i++)
+		{
+			powerResult *= base;
+		}
+					
+		return isNegative ? 1/powerResult : powerResult;
+	}
+		
+		
+	/**
+	* Finds the nth root by approximation
+	* @param base: number to approximate
+	* @param nthRoot: number of times the root needs to be multiplied by itself 
+	* to result the base 
+	* @return the nth root of the base
+	*/
+	private static double root(double base, double nthRoot)
+	{
+		if (nthRoot == 1)
+		{
+			return base;
+		}
 		    
-		    double upperRoot = 1;
-		    double lowerRoot = 0;
+		double upperRoot = 1;
+		double lowerRoot = 0;
 		    
-		    double baseApproximation = 1;
+		double baseApproximation = 1;
 		    
-		    for (double root = 1; baseApproximation < base; root++)
-		    {
-		        for (int i = 0; i < nthRoot; i++)
-	            {
-	                baseApproximation *= root;
-	            }
+		for (double root = 1; baseApproximation < base; root++)
+		{
+			for (int i = 0; i < nthRoot; i++)
+	        	{
+	        		baseApproximation *= root;
+	            	}
 	            
-	            if (baseApproximation == base)
-	            {
-	                return root;
-	            }
+	            	if (baseApproximation == base)
+	            	{
+	                	return root;
+	            	}
 	            
-	            upperRoot = root;
-		    }
+	           	upperRoot = root;
+		}
 	        
 	        lowerRoot = upperRoot - 1;
 	        
-		    return bisectionMethod(upperRoot, lowerRoot, base, nthRoot);
-		}
+		return bisectionMethod(upperRoot, lowerRoot, base, nthRoot);
+	}
 		
 
-		/**
-		 * Approximates the root using Bisection Method
-		 * @param upperRoot: upper bound of the set of numbers that contain the real root
-		 * @param lowerRoot: lower bound of the set of numbers that contain the real root
-		 * @param base: number we are getting the nth root of
-		 * @param nthRoot: number of times the root needs to be multiplied by itself 
-		 * to result the base 
-		 * @return an approximation of the root
-		 */
-		private static double bisectionMethod(double upperRoot, double lowerRoot, 
+	/**
+	* Approximates the root using Bisection Method
+	* @param upperRoot: upper bound of the set of numbers that contain the real root
+	* @param lowerRoot: lower bound of the set of numbers that contain the real root
+	* @param base: number we are getting the nth root of
+	* @param nthRoot: number of times the root needs to be multiplied by itself 
+	* to result the base 
+	* @return an approximation of the root
+	*/
+	private static double bisectionMethod(double upperRoot, double lowerRoot, 
 		                                        double base, double nthRoot)
-		{
-		    double estimatedRoot = 1;
+	{
+		double estimatedRoot = 1;
 		    
-		    while (upperRoot - lowerRoot > EPSILON)
-		    {
-		        double middleBaseApproximation = 1;
+		while (upperRoot - lowerRoot > EPSILON)
+		{
+			double middleBaseApproximation = 1;
 		        double middleRoot = (upperRoot + lowerRoot)/2;
 		    
 		        for (int i = 0; i < nthRoot; i++)
-	            {
-	                middleBaseApproximation  *= middleRoot;
-	            }
+	            	{
+	                	middleBaseApproximation  *= middleRoot;
+	            	}
 	            
-	            if (middleBaseApproximation > base)
-	            {
-	                upperRoot = middleRoot;
-	            }
-	            else
-	            {
-	                lowerRoot = middleRoot;
-	            }
+	          	if (middleBaseApproximation > base)
+	            	{
+	                	upperRoot = middleRoot;
+	            	}
+	            	else
+	            	{
+	                	lowerRoot = middleRoot;
+	            	}
 	            
-	            estimatedRoot = middleRoot;
-		    }
+	            	estimatedRoot = middleRoot;
+		}
 		    
 		    return estimatedRoot;
 		}

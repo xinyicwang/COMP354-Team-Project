5c5ea45b86813c9fbe45d575cfed44eac60753fc
Briana Toia <brianatoia26@gmail.com>
2020-06-03 16:19:12 -0400

    organized the file a bit

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -3,7 +3,7 @@ import java.util.*;
 import function.EPower;
 import function.NRoot;
 import function.Power;
-import function.Power2;
+import function.PowersOfPI;
 import function.Exponentiation;
 import function.StandardDeviation;
 import function.Mean;
@@ -12,43 +12,53 @@ import function.Mean;
 public class CalculatorApp {
 
 	public static void main(String[] args) {
-		
+
+		//Test x^y
 		System.out.println("3^4 = " + Exponentiation.powerInt(3,4));
 		System.out.println("25^3 = " + Exponentiation.powerInt(25,3));
 		System.out.println("2^-2 = " + Exponentiation.powerInt(2,-2));
+
+		//Test e^x
 		//System.out.println(EPower.ePower(3.0 / 2.0));
-		System.out.println(Power.power(16, 0.5));
-				
+		//System.out.println(Power.power(16, 0.5));
+
+		//Test pi^x
+		System.out.println("pi^1 = " + PowersOfPI.powersOfPi(1));
+		System.out.println("pi^2.1 = " + PowersOfPI.powersOfPi(2.1));
+		System.out.println("pi^-1 = " + PowersOfPI.powersOfPi(-1));
+
+		//Test Standard Deviation and Mean
 		List<Double> nums = new ArrayList<Double>();
-        Scanner sc = new Scanner(System.in);
-        // prompt for user input
-        do {
-            System.out.println("Current input is " + nums);
-            System.out.println("Add more? (y/n)");
-            if(sc.next().startsWith("y")) {
-                System.out.println("Enter: ");
-                nums.add(sc.nextDouble());
-            }
-            else {
-                break;
-            }
-        } while(true);
+    Scanner sc = new Scanner(System.in);
+    // prompt for user input
+    do {
+    	System.out.println("Current input is " + nums);
+      System.out.println("Add more? (y/n)");
+      if(sc.next().startsWith("y")) {
+      	System.out.println("Enter: ");
+        nums.add(sc.nextDouble());
+      }
+      else {
+      	break;
+      }
+    } while(true);
 
-        sc.close();
-        System.out.println("List is " + nums);
+    sc.close();
+    System.out.println("List is " + nums);
 
-        // map user input into an array of doubles
-        double[] doubles = new double[nums.size()];
-        for(int i = 0; i < doubles.length; i++) {
-            doubles[i] = nums.get(i);
-        }
-        int n = doubles.length;
+		if (nums.size() != 0) {
+	    // map user input into an array of doubles
+	    double[] doubles = new double[nums.size()];
+	    for(int i = 0; i < doubles.length; i++) {
+	    	doubles[i] = nums.get(i);
+	    }
+	    int n = doubles.length;
 
-        System.out.println("The standard deviation of " + n + " numbers is " + StandardDeviation.standardDeviation(doubles, n));
-        System.out.println("The mean absolute deviation of the numbers is " + Mean.mad(doubles, n));
-	
-		System.out.print(Power2.power(10, 2.1));
-		
+	    System.out.println("The standard deviation of " + n + " numbers is "
+					+ StandardDeviation.standardDeviation(doubles, n));
+	    System.out.println("The mean absolute deviation of the numbers is "
+					+ Mean.mad(doubles, n));
+		}
 	}
 
 }

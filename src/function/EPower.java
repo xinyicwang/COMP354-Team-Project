febbe5d947cd6bd9b140cce00cf5143943dfda29
Thuyohn <thuyohn@gmail.com>
2020-06-04 12:38:12 -0400

    - Organized main and added my function e^ x - Removed my old files.

--- src/function/EPower.java
+++ src/function/EPower.java
@@ -1,10 +1,19 @@
 package function;
 
 public class EPower {
-	
-	public final static double E =  2.718281828459045;
-	
+		
 	public static double ePower(double exponent) {
-		return Power.power(E, exponent);
+        
+		// N is a random number to get the right precision.
+        int N = 1000;
+        //pN will be the polynomial that you calculate iteratively
+        double pN = 1 + exponent/N;
+        
+        // with the use of the Horner's rule to approximated the roots.
+        // Starting from the most innermost brackets. 
+        while (--N >= 1) {
+            pN = 1 + exponent/N*pN;
+        }
+        return pN;
 	}
 }

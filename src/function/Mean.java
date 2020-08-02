c7ee785d4ff641378412eb0c92f552e29041d506
JasonTiu <65509463+JasonTiu@users.noreply.github.com>
2020-08-02 15:27:46 -0400

    Update Mean.java
    Added comments, fixed mad input to only take in array.

--- src/function/Mean.java
+++ src/function/Mean.java
@@ -1,10 +1,32 @@
+/*
+ * Mean Absolute Deviation
+ * Team J - COMP 354
+ * Concordia University 
+ * 
+ * Version: Iteration II
+ *
+ * Due: August 10th 2020
+ * 
+ * Copyright notice
+ */
+
 package function;
 
 import java.util.List;
 
 import gui.MultiVariableEquation;
 
-public class Mean implements MultiVariableEquation {
+/**
+ * Class for calculating the Mean Absolute Deviation.
+ * Has a function that calculates the absolute value of a number, 
+ * and a function that calculates the sum of an array.
+ * @version 31 July 2020
+ * @author Wing-Fei Jason Tiu 40048495
+ */
+
+public class Mean {
+	
+	/*Implement this class by calling Mean.mad(array, array.length)*/
 
 	/**
 	* Function that calculates the absolute value of a number
@@ -14,7 +36,7 @@ public class Mean implements MultiVariableEquation {
 	
 	private static double abs(double num)
 	{
-		return (num <0) ? -num : num;
+		 return (num <0) ? -num : num;
 	}
 	
 	/**
@@ -35,35 +57,35 @@ public class Mean implements MultiVariableEquation {
 	/**
     	* Function that calculates the mean absolute deviation of an array of numbers
      	* @param arr: array of doubles
-     	* @param n: length of the array
      	* @return the mean absolute deviation of the array
      	*/
 		
-	public static double mad(double arr[], int n)
+	public static double mad(double arr[])
 	{
 		 
-	     double mean = sum(arr) / (double) n;
+	     double mean = sum(arr) / arr.length;
 	     
 	     double abdiff = 0;
 	     
-	     for(int i = 0; i < n; i++) {
+	     /*Iterates through the array arr[], finds the absolute value at the index i and subtract the mean value from it.*/
+	     
+	     for(int i = 0; i < arr.length; i++) {
 	            abdiff += abs((arr[i] - mean)) ;
 	        }
 	     
-	     abdiff = (abdiff / (double) n);
+	     abdiff = (abdiff / arr.length);
 	        
 	     return abdiff;
 		
 	}
-
+	
 	@Override
 	public double calculate(List<Double> x) {
-		int size = x.size();
-		double[] items = new double[size];
+		double[] items = new double[x.size()];
 		for(int i = 0; i < x.size(); i ++) {
 			items[i] = x.get(i);
 		}
-		return Mean.mad(items, size);
+		return Mean.mad(items);
 	}
 
 	
